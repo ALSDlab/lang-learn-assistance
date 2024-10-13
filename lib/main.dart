4e72cec86bbd549d8e84235be21283b9e8ae333b
Sungdae <sungdae.yi@gmail.com>
2024-10-13 20:00:34 +0200

    feat: ios deploy 오류 수정

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,10 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await EasyLocalization.ensureInitialized();
   diSetup();
   runApp(
     EasyLocalization(
